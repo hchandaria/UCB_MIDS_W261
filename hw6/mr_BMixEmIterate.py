96fccf8182a8cbce61614c9ed2f154f4fda275a3
Hetal Chandaria <hetalchandaria@gmail.com>
2016-02-26 17:50:23 -0800

    Added HW6

--- hw6/mr_BMixEmIterate.py
+++ hw6/mr_BMixEmIterate.py
@@ -2,8 +2,11 @@ from __future__ import division
 from mrjob.job import MRJob
 
 from math import sqrt, exp, pow,pi
-from numpy import zeros, shape, random, array, zeros_like, dot, linalg
+from numpy import zeros, shape, random, array, zeros_like, dot, linalg, log,exp
+import numpy as np
 import json, re
+import collections
+from decimal import *
 
 WORD_RE = re.compile(r"[\w']+")
 
@@ -11,21 +14,14 @@ WORD_RE = re.compile(r"[\w']+")
 # @alpha = prior probability for cluster k
 # @qmk_local = vocab probability for cluster k
 # return partially calculated  rnk (numerator)
-def bernoulli(terms, alpha,qmk_local,n,k):
-    rnk_present = 0.0
-    rnk_absent = 0.0
-    rnk_local= 0.0
-#     print terms
-#     print qmk_local
+def bernoulli(terms, alpha,qmk_local,n,k,epsilon):
+    lprob = 0.0 #log probability
     for key,value in qmk_local.iteritems():
-        if key in terms:
-#             print "value",value
-            rnk_present += value
-        else:
-            rnk_absent += (1.0-value)
-#     print "N,K,present,absent: ",n,k,rnk_present,rnk_absent
-    rnk_local = alpha*(rnk_present*rnk_absent)
-    return rnk_local
+        if key not in terms: #if we have never seen the term in the document 
+            continue
+        lprob += log(terms[key]*(value + epsilon) + ((1-terms[key] )*(1-value + epsilon)))
+    lprob =  Decimal(log(alpha)+lprob).exp()
+    return lprob
         
         
 
@@ -46,20 +42,23 @@ class MrBMixEm(MRJob):
         
         self.qmk = {int(k):v for k,v in temp.items()}
         
+        self.vocab = []
         self.new_qmk = {}
         for i in range(self.options.k):
             self.new_qmk[i]={}
         for key,value in self.qmk.iteritems():
             for term,val in value.iteritems():
                 self.new_qmk[key][term]=0
-        
-        
+                if(val != 0):
+                    self.vocab.append(term)
+                
+        self.vocab =  set(self.vocab)
+            
         self.new_alphas = zeros_like(self.alpha)        #partial weighted sum of weights
         
         self.numMappers = 1             #number of mappers
         self.count = 0                  #passes through mapper
-        
-        print 'from init:',self.qmk
+
         
     def configure_options(self):
         super(MrBMixEm, self).configure_options()
@@ -75,86 +74,84 @@ class MrBMixEm(MRJob):
         smoothing = 0.0001
         #accumulate partial sums for each mapper
         val = re.findall(WORD_RE,val)
-        stripe ={} 
+        stripe ={k: 0 for k in self.vocab} 
+        
+    
         for word in val:
-            if word in stripe:
+            if word not in stripe:
                 continue
             stripe[word]=1
         rnk=zeros_like(self.alpha)
-        print 'Mapper',self.count,self.qmk
+        
         for i in range(0,self.options.k):
-            rnk[i] = bernoulli(stripe,self.alpha[i],self.qmk[i],self.count,i)
-        rnkSum = sum(rnk)
-        if(rnkSum ==0):
-            rnk=zeros_like(self.alpha)
-        else:
-            rnk = rnk/rnkSum
-        yield self.count, rnk.tolist()
+            rnk[i] = bernoulli(stripe,self.alpha[i],self.qmk[i],self.count,i,smoothing)
+
+        rnk = rnk/sum(rnk)
+   
         #increment counts 
         self.count += 1
         
-#         #accumulate new alpha 
-#         self.new_alphas = self.new_alphas + (rnk + smoothing)
+        #accumulate new alpha 
+        self.new_alphas = self.new_alphas + (rnk + smoothing)
         
-#         for i in range(self.options.k):
-#             for key,value in self.new_qmk[i].iteritems():
-#                 if key not in stripe:
-#                     continue
-#                 self.new_qmk[i][key] = value + (rnk[i] + smoothing)
-        #dummy yield - real output passes to mapper_final in self
+        
+        print "Doc Probability: ",self.count,[round(i,4) for i in rnk]
+        
+        for i in range(self.options.k):
+            for key in val:
+                self.new_qmk[i][key] +=  (rnk[i] + smoothing)
+#         dummy yield - real output passes to mapper_final in self
 
         
-#     def mapper_final(self):
+    def mapper_final(self):
         
-#         out = [self.count, (self.new_alphas).tolist(),self.new_qmk ]
-#         jOut = json.dumps(out)        
-        
-#         yield 1,jOut
+        out = [self.count, (self.new_alphas).tolist(),self.new_qmk ]
+        jOut = json.dumps(out)
+        yield 1,jOut
     
     
-#     def reducer(self, key, xs):
-#         #accumulate partial sums
-#         first = True        
-#         #accumulate partial sums
-#         #xs us a list of paritial stats, including count, phi, mean, and covariance. 
-#         #Each stats is k-length array, storing info for k components
-#         for val in xs:
-#             if first:
-#                 temp = json.loads(val)
-#                 #totCount, totPhi, totMeans, and totCov are all arrays
-#                 totCount = temp[0]
-#                 totalpha = array(temp[1])
-#                 totqmk = {int(k):v for k,v in temp[2].items()}            
-#                 first = False
-#             else:
-#                 temp = json.loads(val)
-#                 #cumulative sum of four arrays
-#                 totCount = totCount + temp[0]
-#                 totalpha = totalpha + array(temp[1])
-#                 local_qmk = {int(k):v for k,v in temp[2].items()}
-#                 for key,value in totqmk.iteritems():
-#                     for term,count in value.iteritems():
-#                         totqmk[key][term] = count + local_qmk[key][term]
+    def reducer(self, key, xs):
+        #accumulate partial sums
+        first = True        
+        #accumulate partial sums
+        #xs us a list of paritial stats, including count, phi, mean, and covariance. 
+        #Each stats is k-length array, storing info for k components
+        for val in xs:
+            if first:
+                temp = json.loads(val)
+                #totCount, totPhi, totMeans, and totCov are all arrays
+                totCount = temp[0]
+                totalpha = array(temp[1])
+                totqmk = {int(k):v for k,v in temp[2].items()}            
+                first = False
+            else:
+                temp = json.loads(val)
+                #cumulative sum of four arrays
+                totCount = totCount + temp[0]
+                totalpha = totalpha + array(temp[1])
+                local_qmk = {int(k):v for k,v in temp[2].items()}
+                for key,value in totqmk.iteritems():
+                    for term,count in value.iteritems():
+                        totqmk[key][term] = count + local_qmk[key][term]
                 
-#         #finish calculation of new probability parameters. array divided by array
-#         newAlpha = totalpha/totCount
-#         #initialize these to something handy to get the right size arrays
-#         newqmk = {}
-#         for i in range(self.options.k):
-#             newqmk[i]={}
-#             for key,value in totqmk[i].iteritems():
-#                 newqmk[i][key] = value/totalpha[i]
+        #finish calculation of new probability parameters. array divided by array
+        newAlpha = totalpha/totCount
+        print array(newAlpha).tolist()
+        #initialize these to something handy to get the right size arrays
+        newqmk = {}
+        for i in range(self.options.k):
+            newqmk[i]={}
+            for key,value in totqmk[i].iteritems():
+                newqmk[i][key] = value/totalpha[i]
             
-#         print newqmk
-#         yield newAlpha, newqmk
-#         outputList = [newAlpha.tolist(),newqmk]
-#         jsonOut = json.dumps(outputList)
+        outputList = [newAlpha.tolist(),newqmk]
+        jsonOut = json.dumps(outputList)
         
-#         #write new parameters to file
-#         fullPath = self.options.pathName + 'intermediateResults_BMM.txt'
-#         fileOut = open(fullPath,'w')
-#         fileOut.write(jsonOut)
-#         fileOut.close()
+        #write new parameters to file
+        fullPath = self.options.pathName + 'intermediateResults_BMM.txt'
+        fileOut = open(fullPath,'w')
+        fileOut.write(jsonOut)
+        fileOut.close()
 
 if __name__ == '__main__':
     MrBMixEm.run()
